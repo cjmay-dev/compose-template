22cfb66f0835e97b09ec41ed21b9079262ba668b
C.J. May <lawndoc@protonmail.com>
2025-06-29 16:45:51 -0500

    remove defaults to highlight missing vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,25 +1,21 @@
 variable APP_SHORTNAME {
     description = "Subdomain where the app is hosted"
     type        = string
-    default     = "appshortname"
 }
 
 variable "ORG_SHORTNAME" {
     description = "Single-word name of organization"
     type        = string
-    default     = "orgshortname"
 }
 
 variable "ENV_SLUG" {
     description = "Environment slug"
     type        = string
-    default     = "prod"
 }
 
 variable "GITHUB_REPOSITORY" {
     description = "GitHub repository in the format 'owner/repo'"
     type        = string
-    default     = "owner/repo"
 }
 
 variable "INFISICAL_PROJECT_SLUG" {
